9ad9e068178f58d1acc1876fa552dd67d1346486
ShivakumarSwamy <shivakumarswamy.bg@gmail.com>
2021-05-16 11:48:21 +0200

    db: validation

---
+++ database/spring-data-jpa-constraint/src/main/java/com/example/entities/Reservation.java
@@ -0,0 +1,79 @@
+package com.example.entities;
+
+import java.util.Calendar;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Future;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "reservation")
+public class Reservation {
+
+    @Id
+    private Long id;
+
+    @NotNull
+    @Size(min = 1, max = 30)
+    private String firstName;
+
+    @NotNull
+    @Size(min = 1, max = 30)
+    private String lastName;
+
+    @NotNull
+    @Pattern(regexp="[A-Z]{3}[0-9]{3}")
+    private String flightNumber;
+
+    @NotNull
+    @Future
+    private Calendar flightDate;
+
+    public Reservation() {
+        // intentionally left empty
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFlightNumber() {
+        return flightNumber;
+    }
+
+    public void setFlightNumber(String flightNumber) {
+        this.flightNumber = flightNumber;
+    }
+
+    public Calendar getFlightDate() {
+        return flightDate;
+    }
+
+    public void setFlightDate(Calendar flightDate) {
+        this.flightDate = flightDate;
+    }
+}
